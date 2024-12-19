156df7264d9c3a4fe2f9992fad976ff09d4e53bb
Jimyeong Kim <kjm021221@icloud.com>
2024-12-19 20:55:08 +0900

    first commit

---
+++ src/test/java/com/project/zerodowntimedeplyoment/ZeroDowntimeDeplyomentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zerodowntimedeplyoment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDowntimeDeplyomentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
